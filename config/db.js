dbe7b81e4ac8dd91e480ba9c5a4b8a02b882c8b8
Kbailey28 <k.bailey371@gmail.com>
2020-06-16 15:43:36 -0400

    routes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectdb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Mongodb connected...');
+  } catch (err) {
+    console.error(err.message);
+    //if fails exit
+    process.exit(1);
+  }
+};
+
+module.exports = connectdb;
